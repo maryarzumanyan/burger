eca231b1b9b49093642cf4793670ac7c763474a8
Mary Arzumanyan <mary.arzumanyan@gmail.com>
2019-01-31 23:17:10 -0800

    Prepare connectivity from Heroku to JawsDB MySQL

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,18 @@
 var result = require("dotenv").config({ path: './config/.env' });
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    port: 3306,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME
-})
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+      host: process.env.DB_HOST,
+      port: 3306,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME
+  });
+}
 
 connection.connect(function(err) {
     if (err) {
